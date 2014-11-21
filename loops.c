4b97d99f9655cf777469e7f8ac01fed990d81dc2
geoyogesh <geoyogesh@gmail.com>
2014-11-20 19:31:17 -0600

    day3

---
+++ loops.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*working with loops*/
+
+int main()
+{
+int i=0;
+while(i<2)
+{
+printf("true\n");
+i++;
+}
+printf("done");
+return 0;
+}
---
+++ pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*working with pointers*/
+
+int main()
+{
+/*
+memory allocated to arrays:
+a consecutively allocated memory boxes of same size
+*/
+return 0;
+}
+
